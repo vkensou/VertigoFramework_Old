1810a9c61f147469d970b14f5d5d6a925bbcc238
vkensou <vkensou@local.com>
2019-02-12 22:14:59 +0800

    Add template folders.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class GameController : MonoBehaviour
+{
+    public bool isEditor;
+    public string bundleConfigName;
+
+    IProcedureManager m_procedureManager;
+    UIManager m_uiManager;
+    SoundManager m_soundManager;
+    ResourceBundleManager m_bundleManager;
+    bool couldUpdate = false;
+
+    void Start()
+    {
+        m_bundleManager = new ResourceBundleManager(isEditor);
+
+        var uiRootObj = GameObject.Find("UIRoot");
+        DontDestroyOnLoad(uiRootObj);
+        m_uiManager = new UIManager(uiRootObj);
+
+        var audioSourceObj = GameObject.Find("SoundSource").GetComponent<AudioSource>();
+        m_soundManager = new SoundManager(audioSourceObj);
+
+        InitialProcedureManager();
+
+        m_bundleManager.LoadResourceBundleConfig(bundleConfigName)
+            .Subscribe(_ => { couldUpdate = true; m_procedureManager.Start(); });
+    }
+
+    void Update()
+    {
+        m_bundleManager.Update();
+        if (couldUpdate)
+            m_procedureManager.Update();
+    }
+
+    void InitialProcedureManager()
+    {
+        m_procedureManager = new EntitasProcedureManager();
+        var hello = new HelloProcedure();
+        m_procedureManager.AddProcedure(hello);
+
+        m_procedureManager.SetInitialProcedure(hello);
+    }
+}
---
+++ Assets/Scripts/Procedures/HelloProcedure.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UML;
+using UniRx;
+
+public class HelloProcedure : SimpleProcedure
+{
+    public HelloProcedure()
+        : base("Hello")
+    {
+
+    }
+
+    public override ISystemEventRoute EventRoute => null;
+
+    public override StateMachine ProcedureStateMachine => null;
+
+    public override Blackboard Blackboard => null;
+}
